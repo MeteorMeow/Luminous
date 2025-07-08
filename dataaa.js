909832e20c0631a62ae09db7c7b185bea733781e
meteormeow <rohitsanjaysinha@gmail.com>
2025-07-08 15:57:20 +0000

    Update dataaa.js

--- dataaa.js
+++ dataaa.js
@@ -1,5 +1,5 @@
 export const items=[
-  {name:"Blue Glasses",img:"/images/blue.jpeg",price:"Rs 499"},
-  {name:"Red Glasses",img:"/images/red.jpeg",price:"Rs 599"},
-  {name:"Green Glasses",img:"/images/green.jpeg",price:"Rs 399"}
+  {name:"Blue Glasses",img:"images/blue.jpeg",price:"Rs 499"},
+  {name:"Red Glasses",img:"images/red.jpeg",price:"Rs 599"},
+  {name:"Green Glasses",img:"images/green.jpeg",price:"Rs 399"}
 ];
